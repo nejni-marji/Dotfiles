6aa2e619306aa0ea7519b662bcf62d642afb6fef
nejni-marji <nejni.marji@gmail.com>
2023-09-18 00:23:38 -0500

    Dotfiles: [2023-09-18 00:23]

--- extras/kitty-scratchpad.zsh
+++ extras/kitty-scratchpad.zsh
@@ -1,35 +1,32 @@
+#!/bin/zsh
 # vim: syn=hash cms=#\ %s tw=80
-#
-# init-kitty-scratchpad.zsh
-#
-
 
 
 # tmux music
-music=music-demo
-if ! tmux has-session -t $music >/dev/null 2>/dev/null ; then
-	tmux new-session -t $music -s $music -d
-	tmux send-keys -t $music:0.0 "msh mpc" enter C-l enter
-	tmux split-window -hd -t $music:0.0
-	tmux send-keys -t $music:0.1 "mpc-display" enter
+MUSIC=music
+if ! tmux has-session -t $MUSIC >/dev/null 2>/dev/null ; then
+	tmux new-session -t $MUSIC -s $MUSIC -d
+	tmux send-keys -t $MUSIC:0.0 "msh mpc" enter C-l enter
+	tmux split-window -hd -t $MUSIC:0.0
+	tmux send-keys -t $MUSIC:0.1 "mpc-display" enter
 fi
 
 
 
 # tmux youtube
-youtube=youtube-demo
-if ! tmux has-session -t $youtube >/dev/null 2>/dev/null ; then
+YOUTUBE=youtube
+if ! tmux has-session -t $YOUTUBE >/dev/null 2>/dev/null ; then
 	mkdir -p /tmp/ytdl
-	tmux new-session -t $youtube -s $youtube -d
+	tmux new-session -t $YOUTUBE -s $YOUTUBE -d
 fi
 
 
 
 # kitty window
-scratchpad=scratchpad-demo
+SCRATCHPAD=scratchpad
 sleep 0.5
 kitty \
-	--title $scratchpad \
+	--title $SCRATCHPAD \
 	--session sessions/scratchpad.conf \
 	-o background_opacity=1 \
 	--start-as minimized \
