70cab6e0a6825ad539fa021647dafd097eb0d748
nejni-marji <nejni.marji@gmail.com>
2023-10-07 23:09:30 -0500

    Dotfiles: [2023-10-07 23:09]

--- extras/bin/opensub-fix.zsh
+++ extras/bin/opensub-fix.zsh
@@ -4,8 +4,17 @@ data=~/Dotfiles/extras/misc/opensubtitles_data.txt
 
 for i in *.srt ; do
 	if ! [[ -f .$i ]] ; then
-		< $i | grep -Fivf $data > tmp
-		mv -v $i .$i ; mv tmp $i
+		if [[ $1 == demo ]] ; then
+			< $i | grep -Fif $data
+			echo
+		else
+			echo "fixed: $i"
+			< $i | grep -Fivf $data > tmp
+			mv $i .$i ; mv tmp $i
+		fi
+	elif [[ $1 != demo ]] ; then
+		echo "ignore: $i"
+
 	fi
 done
 
