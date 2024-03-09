0929af2b0d38b792520a08ca737b7704e1c4fa35
nejni-marji <nejni.marji@gmail.com>
2024-03-08 22:19:57 -0600

    Dotfiles: [2024-03-08 22:19]

--- zsh/zsh-mommy.zsh
+++ zsh/zsh-mommy.zsh
@@ -1,50 +1,57 @@
 #!/bin/zsh
 
 # use a lockfile to enable/disable mommy
-ZSH_MOMMY_LOCK=~/.zsh/mommy.lck
+ZMOMMY_LOCK=~/.zsh/mommy.lck
 
 # very basic hook setup
 autoload -Uz add-zsh-hook || return
 
 # find mommy
-if ! [[ -v ZSH_MOMMY_BIN ]] ; then
+if ! [[ -v ZMOMMY_BIN ]] ; then
 	if which cargo-mommy >/dev/null 2>/dev/null ; then
-		ZSH_MOMMY_BIN=cargo-mommy
+		ZMOMMY_BIN=cargo-mommy
 	else
-		ZSH_MOMMY_BIN=~/.cargo/bin/cargo-mommy
+		ZMOMMY_BIN=~/.cargo/bin/cargo-mommy
 	fi
 	# if we don't have an executable mommy, publish a warning
-	if ! [[ -x $ZSH_MOMMY_BIN ]] ; then
-		echo 'zsh-mommy: unable to find cargo-mommy'
-		ZSH_MOMMY_BIN=true
+	if ! [[ -x $ZMOMMY_BIN ]] ; then
+		echo 'zmommy: unable to find cargo-mommy'
+		ZMOMMY_BIN=true
 	fi
 fi
+
+# zmommy_setup() {
+# 	zmommy_lastcmd=$1
+# }
 
 # this variable tells mommy what to run. by running true/false, we just force
 # the output to be whatever we want as quickly as possible. there is no need to
 # get a static mommy binary when you can just point mommy at true/false.
-zsh-mommy() {
+zmommy() {
 err=$?
-[[ -f $ZSH_MOMMY_LOCK ]] || return
+[[ -f $ZMOMMY_LOCK ]] || return
 if [[ $err -eq 0 ]] ; then
-	CARGO_MOMMYS_ACTUAL=true $ZSH_MOMMY_BIN
+	CARGO_MOMMYS_ACTUAL=true $ZMOMMY_BIN
 else
-	CARGO_MOMMYS_ACTUAL=false $ZSH_MOMMY_BIN
+	CARGO_MOMMYS_ACTUAL=false $ZMOMMY_BIN
 fi
 }
 
 # add the hook
-add-zsh-hook precmd zsh-mommy
+# add-zsh-hook preexec zmommy_setup
+add-zsh-hook precmd zmommy
 
 mommy() {
-	if ! [[ -f $ZSH_MOMMY_LOCK ]] ; then
-		touch $ZSH_MOMMY_LOCK
+	if ! [[ -f $ZMOMMY_LOCK ]] ; then
+		touch $ZMOMMY_LOCK
 		echo 'mommy enabled'
-		# add-zsh-hook precmd zsh-mommy
+		# add-zsh-hook preexec zmommy_setup
+		# add-zsh-hook precmd zmommy
 	else
-		\rm $ZSH_MOMMY_LOCK
+		\rm $ZMOMMY_LOCK
 		echo 'mommy disabled'
-		# add-zsh-hook -d precmd zsh-mommy
+		# add-zsh-hook -d preexec zmommy_setup
+		# add-zsh-hook -d precmd zmommy
 	fi
 }
 
@@ -54,13 +61,15 @@ mommy() {
 
 # if you tell mommy "no", she'll go away in that shell
 no-mommy() {
-	add-zsh-hook -d precmd zsh-mommy
+	# add-zsh-hook -d preexec zmommy_setup
+	add-zsh-hook -d precmd zmommy
 }
 
 # if you tell mommy "yes", on the other paw...
 yes-mommy() {
-	touch $ZSH_MOMMY_LOCK
-	add-zsh-hook precmd zsh-mommy
+	touch $ZMOMMY_LOCK
+	# add-zsh-hook preexec zmommy_setup
+	add-zsh-hook precmd zmommy
 }
 
 # function to unset all of mommy's configurations (for demo purposes)
