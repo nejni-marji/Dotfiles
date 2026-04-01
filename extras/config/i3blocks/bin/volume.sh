e9da976c27bde10ba11ab797294f65910cf2633a
nejni-marji <nejni.marji@gmail.com>
2026-04-01 06:41:58 -0500

    Dotfiles: [2026-04-01 06:41]

--- extras/config/i3blocks/bin/volume.sh
+++ extras/config/i3blocks/bin/volume.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+case $BLOCK_BUTTON in
+	4)
+		volume=$(pactl --format json get-sink-volume @DEFAULT_SINK@ | jq -r '[.volume[].value_percent | .[:-1] | tonumber] | max')
+		[[ $volume -ge 100 ]] ||
+		pactl set-sink-volume @DEFAULT_SINK@ +5%
+		;;
+	5)
+		pactl set-sink-volume @DEFAULT_SINK@ -5%
+		;;
+	3)
+		pactl set-sink-mute @DEFAULT_SINK@ toggle
+		#pactl set-source-mute @DEFAULT_SOURCE@ toggle
+		;;
+esac
+
 volume=$(pactl --format json get-sink-volume @DEFAULT_SINK@ | jq -r '[.volume[].value_percent | .[:-1] | tonumber] | max')
 
 mute=$(pactl --format json get-sink-mute @DEFAULT_SINK@ | jq -r .mute)
