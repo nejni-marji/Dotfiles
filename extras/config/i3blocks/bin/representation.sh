11589e8c51e2ead05fefcb28ca554adb22bc0f9d
nejni-marji <nejni.marji@gmail.com>
2026-04-01 13:22:23 -0500

    Dotfiles: [2026-04-01 13:22]

---
+++ extras/config/i3blocks/bin/representation.sh
@@ -0,0 +1,13 @@
+while swaymsg -t get_workspaces |
+	jq -cr '..
+	| select(.focused? and .type=="workspace").representation
+	| if .==null then
+	"•" else
+		gsub("(?<=\\[| )[\\w_-]+(?!\\[) ?"; "•")
+		| gsub("(?<m>•{4,})"; "\(.m | length)")
+	end'
+	true
+do swaymsg -t subscribe \
+	'["window", "workspace", "output", "binding"]' \
+	>/dev/null 2>&1
+done
