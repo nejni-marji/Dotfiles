311f7f46088b051724e75876539172fc8c6cf7e6
nejni-marji <nejni.marji@gmail.com>
2026-04-04 15:44:01 -0500

    Dotfiles: [2026-04-04 15:44]

--- extras/config/i3blocks/bin/battery.sh
+++ extras/config/i3blocks/bin/battery.sh
@@ -5,21 +5,29 @@ acpi -b |
 perl -pe '
 s/^Battery (\d+): ([^,]+), (\d+%)(?:, ([\d:]+?):\d\d )?.*/<span foreground="#\2">\3<\/span> (~\4)/;
 s/Not charging/808080/;
-s/Discharging/ff0000/;
+s/Discharging/ffff00/;
 s/Charging/00ff00/;
 s/Full/808080/;
-s/ \(~\)//;' |
-tr '\n' , |
-perl -pe 's/,/, /g; s/, $/\n/'
+s/ \(~\)//;
+s/\n/, /;
+' |
+perl -pe '
+s/, ?$/\n/;
+s#</span>, #,</span> #g;
+'
 
 # short
 acpi -b |
 perl -pe '
 s/^Battery (\d+): ([^,]+), (\d+%)(?:, ([\d:]+?):\d\d )?.*/<span foreground="#\2">\3<\/span>/;
 s/Not charging/808080/;
-s/Discharging/ff0000/;
+s/Discharging/ffff00/;
 s/Charging/00ff00/;
 s/Full/ffffff/;
-s/ \(~\)//;' |
-tr '\n' , |
-perl -pe 's/,/, /g; s/, $/\n/'
+s/ \(~\)//;
+s/\n/, /;
+' |
+perl -pe '
+s/, ?$/\n/;
+s#</span>, #,</span> #g;
+'
