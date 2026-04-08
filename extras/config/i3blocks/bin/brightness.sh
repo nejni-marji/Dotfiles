c360b6714ec7dc560540a408521bcedf66aa6122
nejni-marji <nejni.marji@gmail.com>
2026-04-08 18:50:53 -0500

    Dotfiles: [2026-04-08 18:50]

--- extras/config/i3blocks/bin/brightness.sh
+++ extras/config/i3blocks/bin/brightness.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 case $BLOCK_BUTTON in
+	3)
+		b1=$(brightnessctl | grep -Po '\d+(?=%)')
+		b2=$(( ($b1+3)/5*5 ))
+		brightnessctl set $b2% >/dev/null
+		;;
 	4)
 		brightnessctl set -- +5% >/dev/null
 		;;
