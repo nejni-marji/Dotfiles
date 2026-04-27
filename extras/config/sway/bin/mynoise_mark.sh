83c71d952c0b65ed3c3361ba2182fa40c50eff74
nejni-marji <nejni.marji@gmail.com>
2026-04-27 09:49:43 -0500

    Dotfiles: [2026-04-27 09:49]

--- extras/config/sway/bin/mynoise_mark.sh
+++ extras/config/sway/bin/mynoise_mark.sh
@@ -6,6 +6,7 @@ if swaymsg -t get_marks | jq -e 'contains(["mynoise"])' ; then
 	if swaymsg -t get_tree | jq -e '.. | select(.focused?).marks | contains(["mynoise"])' ; then
 		swaymsg move scratchpad
 	else
+		swaymsg '[con_mark="^mynoise$"] move scratchpad'
 		swaymsg '[con_mark="^mynoise$"] focus'
 	fi
 else
