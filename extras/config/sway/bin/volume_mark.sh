7b4fb8b909d18bd3a568229d823cdca657e6acb0
nejni-marji <nejni.marji@gmail.com>
2026-04-27 05:11:43 -0500

    Dotfiles: [2026-04-27 05:11]

---
+++ extras/config/sway/bin/volume_mark.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if swaymsg -t get_marks | jq -e 'contains(["vol"])' ; then
+	if swaymsg -t get_tree | jq -e '.. | select(.focused?).marks | contains(["vol"])' ; then
+		swaymsg scratchpad show
+	else
+		swaymsg '[con_mark="^vol$"] focus'
+	fi
+else
+	swaymsg 'exec $term -T pulsemixer pulsemixer'
+fi
