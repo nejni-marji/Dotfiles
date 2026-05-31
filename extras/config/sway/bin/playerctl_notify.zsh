0f10b04f54b9c3ef194d561b8cc59727143bcffd
nejni-marji <nejni.marji@gmail.com>
2026-05-31 15:55:22 -0500

    Dotfiles: [2026-05-31 15:55]

--- extras/config/sway/bin/playerctl_notify.zsh
+++ extras/config/sway/bin/playerctl_notify.zsh
@@ -7,4 +7,11 @@ artist="$(playerctl -p "$player" metadata --format '{{artist}}')"
 title="$(playerctl -p "$player" metadata --format '{{title}}')"
 typeset -i volume=$((100*"$(playerctl -p "$player" volume)"))
 
-notify-send "${title}" "${album}${album:+${artist:+ by }}${artist}${${:-$artist$album}:+\\\n}${volume}%"
+typeset album
+typeset artist
+typeset title
+typeset volume
+
+n="\n"
+
+notify-send "${title}" "${album}${album:+${artist:+ by }}${artist}${${:-$artist$album}:+ (}${volume}%${${:-$artist$album}:+)}"
