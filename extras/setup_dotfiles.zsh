8be6a688d48568f67514245b56a5a4c82b77525b
nejni-marji <nejni.marji@gmail.com>
2026-06-02 11:33:11 -0500

    Dotfiles: [2026-06-02 11:33]

--- extras/setup_dotfiles.zsh
+++ extras/setup_dotfiles.zsh
@@ -8,22 +8,26 @@ fi
 
 cd Dotfiles
 for i in * ; do
-	if [[ $i == extras ]] || [[ $i == config ]] ; then
-		continue
-	fi
-	ln -srfnv $i ~/.$i
+	case $i in
+		extras|bin|config)
+			continue
+			;;
+		*)
+			ln -srfnv $i ~/.$i
+			;;
+	esac
 done
 
-cd ~/Dotfiles/extras
+cd ~/Dotfiles
 ln -srfnv bin ~/bin
 
-cd ~/Dotfiles/extras/config
+cd ~/Dotfiles/config
 mkdir -v ~/.config
 for i in * ; do
 	ln -srfnv $i ~/.config/$i
 done
 
-cd ~/Dotfiles/extras/config
+cd ~/Dotfiles/config
 mkdir -pv nvim/data/backup/$HOST
 mkdir -pv nvim/data/swap/$HOST
 mkdir -pv nvim/data/undo/$HOST
@@ -31,11 +35,11 @@ mkdir -pv nvim/data/undo/$HOST
 cd ~/Dotfiles
 host_suffixed=(
 	# remember to update the .gitignore in tandem with this
-	extras/config/foot/foot.ini
-	extras/config/i3blocks/bin/date_fuzzy_emoji_clock.py
-	extras/config/i3blocks/config
-	extras/config/mako/config
-	extras/config/sway/config.d/host-include
+	config/foot/foot.ini
+	config/i3blocks/bin/date_fuzzy_emoji_clock.py
+	config/i3blocks/config
+	config/mako/config
+	config/sway/config.d/host-include
 )
 
 cd ~/Dotfiles
