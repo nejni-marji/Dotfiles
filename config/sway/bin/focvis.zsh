e915b55db410e34909ea11156c3fa0a48ed4b65e
nejni-marji <nejni.marji@gmail.com>
2026-08-23 10:12:54 -0500

    Dotfiles: [2026-08-23 10:12]

---
+++ config/sway/bin/focvis.zsh
@@ -0,0 +1,68 @@
+#!/bin/zsh
+
+# store to $dir or die
+[[ $1 =~ 'up|down|left|right' ]] && dir=$1 || exit 10
+
+id=$(swaymsg -t get_tree | jq -r --arg dir $dir '
+
+. as $tree |
+
+{
+	"left": "splith",
+	"right": "splith",
+	"up": "splitv",
+	"down": "splitv"
+}[$dir] as $good_layout |
+
+{
+	"left": 0,
+	"right": 1,
+	"up": 0,
+	"down": 1,
+}[$dir] as $dir_sign |
+
+def get_good_parent:
+	if .layout==$good_layout then . else
+		.id as $prev_id |
+		$tree | .. |
+		select(.nodes? | length!=0) |
+		select(.nodes | map(.id) | contains([$prev_id])) |
+		get_good_parent
+	end
+;
+
+# get correct parent of focused with recursive function
+$tree | .. | select(.focused?) | get_good_parent |
+
+# parent aka parent, source aka highest
+. as $parent |
+.id as $parent_id |
+.focus[0] as $child_id |
+
+#"child \($child_id) parent \($parent_id)"
+
+# next, we need to get the target neighbor of $child_id
+
+$parent.nodes |
+	map(.id) as $nodes | length as $length |
+	$nodes | index($child_id) |
+	. as $index |
+	if $index == $dir_sign*($length-1) then empty else
+		$nodes[$index + [-1, 1][$dir_sign]]
+	end |
+	. as $neighbor_id |
+
+def get_lowest_child:
+	if .focus == [] then . else
+		.focus[0] as $foc |
+		.nodes | map(select(.id==$foc))[0] | get_lowest_child
+	end
+;
+
+$parent | .nodes |
+	map(select(.id?==$neighbor_id))[0] |
+	get_lowest_child |
+	.id
+')
+
+swaymsg "[con_id=$id] focus"
