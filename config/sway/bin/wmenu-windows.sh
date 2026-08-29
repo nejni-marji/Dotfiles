10b852779b22ecd73c9a0bc57765132d806e9a36
nejni-marji <nejni.marji@gmail.com>
2026-08-29 05:13:30 -0500

    Dotfiles: [2026-08-29 05:13]

--- config/sway/bin/wmenu-windows.sh
+++ config/sway/bin/wmenu-windows.sh
@@ -19,7 +19,7 @@ window=$(swaymsg -t get_tree | jq -r '
 		if .app_id then .app_id else .window_properties.class end
 	))"
 ' |
-wmenu -i -l 15 -f "$font" -p 'Windows:' \
+wmenu -i -l 10 -f "$font" -p 'Windows:' \
 	-M ff8cda -m 000000 -N 8cdaff -n 000000 -S ff8cda -s 000000 \
 )
 
